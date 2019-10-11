8fe3a76aa46a2798ac4bc1777a0598ac3030118e
ms374-HW <55681191+ms374-HW@users.noreply.github.com>
2019-10-11 12:11:45 +0400

    Changing the canvas to DIV and javascript to Jquery

--- mainPERSONAL.js
+++ mainPERSONAL.js
@@ -2,14 +2,27 @@ var ghost = new Image();
  var grave = new Image();
  var knight = new Image();
  var background = new Image();
-
+ 
  knight.src = "http://gameartpartners.com/wp-content/uploads/edd/2015/03/Darkness_Knight_Featured.png";
  ghost.src = "https://www.freeiconspng.com/uploads/simple-cartoon-ghost-png-30.png";
  grave.src = "https://cdn.pixabay.com/photo/2014/04/03/11/54/headstone-312540_640.png";
  background.src="https://s3.envato.com/files/234687569/Halloween%20Game%20Background/Halloween%20BG%2002/TileAble%20Halloween%20Background%2002.png";
-
  
+ var canvas = document.getElementById("myCanvas");
+ var context = canvas.getContext("2d");
+ 
+gameBegin = function () {
+    context.font = "30px EvilFont";
+    context.beginPath();
+    context.fillRect(0, 0, 800, 500);
+    context.fillStyle = "#ffffff";
+    context.fillText("Begin Game", 110, 80);
+    context.fill();
+    context.closePath();
 
+    document.addEventListener("mousedown", draw);
+
+}
 
 
 //Storing the moves of a grave in an array of classes
@@ -28,20 +41,18 @@ var knightMoves = {
     jumpVelocity : 0
 };
 
- 
- var canvas = document.getElementById("myCanvas");
- var context = canvas.getContext("2d");
+
 
 
 //Controller Class
 var controller = {
     up: false,
 
-    keyListener: function(event) {
+    isItJumping: function(event) {
         var state = (event.type == "keydown") ? true : false;
         switch(event.keyCode) {
             case 32 :                           //32 = space bar //38 = up arrow
-            case 38 : //controller.up = state
+            case 38 : controller.up = state;
                       jump();
                       break;
         }
@@ -49,24 +60,36 @@ var controller = {
     }
 };
 
-jump = function() {
+jump = function() {    
 
-    //if(controller.up && knightMoves.jumping == false){
-        knightMoves.jumpVelocity -= 20;
+    if(controller.up && knightMoves.jumping == false){
+        knightMoves.jumpVelocity -= 60;
+        knightMoves.kX += 5;
         knightMoves.jumping = true;
        console.log("Jumping");
-    //} 
+    } 
 
     //Gravity should bring the knight down
-    knightMoves.jumpVelocity += 1.5;
+    knightMoves.jumpVelocity += 0.5;
 
     //Knight's position
     knightMoves.kY += knightMoves.jumpVelocity;
+    knightMoves.kX += 0.5;
 
     //Friction and gravity slows it down as well
-    knightMoves.jumpVelocity *= 0.9;
+    
+    knightMoves.jumpVelocity *= 0.2;
 
+    if(knightMoves.kY > 100){
+        knightMoves.jumping = false;
+        knightMoves.velocity = 0;
+        knightMoves.kY = 100;
+        knightMoves.kX = 10;
+    }
+    
+   
     requestAnimationFrame(jump);
+    
 } 
 
 
@@ -102,8 +125,8 @@ draw = function(){
 
 
 
-document.addEventListener("keydown", jump);
-    
+document.addEventListener("keydown", controller.isItJumping);
+document.addEventListener("keyup", controller.isItJumping);
 
 
 //if the gamer presser up or space bar the knight must jump to avoid the obstacles
